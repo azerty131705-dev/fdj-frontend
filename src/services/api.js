4d454368c1b46388903461890e68647f7ac84c1d
Azerty131705_dev <Azerty131705@gmail.com>
2025-10-24 13:42:47 +0200

    Fix API URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,9 @@
 ﻿import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://fdj-backend-1.onrender.com", // ✅ ton backend Render
+    baseURL: "https://fdj-backend-1.onrender.com", // ✅ ton backend Render
 });
 
 export default api;
+
 
